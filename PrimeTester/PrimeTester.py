60160b20218d2d3926d97477a9910312d60c9bad
stefanmzeidler <szeidler@uwm.edu>
2024-03-12 23:58:18 -0500

    I think smallest_r is working now.

--- PrimeTester/PrimeTester.py
+++ PrimeTester/PrimeTester.py
@@ -7,7 +7,6 @@ class PrimeTester:
     @staticmethod
     def perfectPower(p):
         # print('Testing the number: ' + str(p))
-        c = 2.0
         b = mpmath.mpf(2.0)
         while b <= mpmath.log(p, 2):
             # print('b is currently ' + str(b))
@@ -18,9 +17,57 @@ class PrimeTester:
             b += 1
         return False
 
-# n = 2
-# while n < 1000:
-#     if not PrimeTester.perfectPower(math.pow(n, n)):
-#         print("The value " + str(n) + " failed when it shouldn't have.")
-#         break
-#     n += 1
+    @staticmethod
+    def smallest_r(n):
+    # returns false if not prime, continue onto next step
+        maxk = mpmath.floor(mpmath.power(mpmath.log(n, 2), 2))
+        # print(str(maxk))
+        term2 = mpmath.log(n, 2)
+        term3 = mpmath.power(term2, 5)
+        term4 = mpmath.ceil(term3)
+        maxr = PrimeTester.mpmax(3, term4)
+        # print(str(maxr))
+        nextr = True
+        r = mpmath.ceil(mpmath.power(term2, 2))
+        while nextr:  # and r < maxr
+            nextr = False
+            if not PrimeTester.gcd(r, n) == 1:
+                continue
+            k = 1
+            while (not nextr) and k <= maxk:
+
+                part1 = mpmath.power(n, k)
+                # print("part 1: " + str(part1))
+                part2 = mpmath.fmod(part1, r)
+                # print("part 2: " + str(part2))
+                # print(str(part2 == 1))
+                # print(str(part2 == 0))
+                if part2 == 1.0 or part2 == 0.0:
+                    nextr = True
+                k += 1
+            r += 1
+        r = r - 2
+        if n <= r:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def polynomialRemainder(a,b):
+        mpmath.poly
+    @staticmethod
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    @staticmethod
+    def mpmax(x, y):
+        if mpmath.fsub(x, y) > 0:
+            return x
+        else:
+            return y
+
+
+
+print(str(PrimeTester.smallest_r(37)))
--- UnitTests/UnitTester.py
+++ UnitTests/UnitTester.py
@@ -5,26 +5,35 @@ import mpmath
 
 
 class MyTestCase(unittest.TestCase):
-    def test_perfectPower_small(self):
-        self.assertTrue(PrimeTester.PrimeTester.perfectPower(8))
-        self.assertTrue(PrimeTester.PrimeTester.perfectPower(3125))
-        self.assertFalse(PrimeTester.PrimeTester.perfectPower(7))
-        self.assertTrue(PrimeTester.PrimeTester.perfectPower(343))
-        self.assertTrue(PrimeTester.PrimeTester.perfectPower(823543))
+    # def test_perfectPower_small(self):
+    #     self.assertTrue(PrimeTester.PrimeTester.perfectPower(8))
+    #     self.assertTrue(PrimeTester.PrimeTester.perfectPower(3125))
+    #     self.assertFalse(PrimeTester.PrimeTester.perfectPower(7))
+    #     self.assertTrue(PrimeTester.PrimeTester.perfectPower(343))
+    #     self.assertTrue(PrimeTester.PrimeTester.perfectPower(823543))
+    #
+    # def test_perfectPower_large(self):
+    #     self.assertTrue(PrimeTester.PrimeTester.perfectPower(math.pow(25, 25)))
+    #     self.assertFalse(PrimeTester.PrimeTester.perfectPower(math.pow(25, 25)) - 1)
+    #     self.assertTrue(PrimeTester.PrimeTester.perfectPower(math.pow(100, 100)))
+    #     self.assertFalse(PrimeTester.PrimeTester.perfectPower(mpmath.power(1001, 1001))-1)
+    #
+    # def test_perfectPower_loop(self):
+    #     n = 2
+    #
+    #     while n < 100:
+    #         print('Testing the number: ' + str(n))
+    #         self.assertTrue(PrimeTester.PrimeTester.perfectPower(mpmath.power(n, n)), "The value " + str(n) + " failed")
+    #         n += 1
 
-    def test_perfectPower_large(self):
-        self.assertTrue(PrimeTester.PrimeTester.perfectPower(math.pow(25, 25)))
-        self.assertFalse(PrimeTester.PrimeTester.perfectPower(math.pow(25, 25)) - 1)
-        self.assertTrue(PrimeTester.PrimeTester.perfectPower(math.pow(100, 100)))
-        self.assertFalse(PrimeTester.PrimeTester.perfectPower(mpmath.power(1001, 1001))-1)
+    # def test_mpmath(self):
+    #     self.assertEqual(mpmath.mpf(1.0),1)
 
-    def test_perfectPower_loop(self):
-        n = 2
+    def test_smallestr(self):
+        self.assertFalse(PrimeTester.PrimeTester.smallest_r(31))
+        self.assertFalse(PrimeTester.PrimeTester.smallest_r(37))
 
-        while n < 10000:
-            print('Testing the number: ' + str(n))
-            self.assertTrue(PrimeTester.PrimeTester.perfectPower(mpmath.power(n, n)), "The value " + str(n) + " failed")
-            n += 1
+
 
 
 if __name__ == '__main__':
