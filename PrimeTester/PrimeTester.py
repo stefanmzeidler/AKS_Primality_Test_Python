c797df74bebf06ef693817ef96b074d65a11199e
stefanmzeidler <szeidler@uwm.edu>
2024-03-12 21:01:21 -0500

    First try

--- PrimeTester/PrimeTester.py
+++ PrimeTester/PrimeTester.py
@@ -1,14 +1,26 @@
 import math
+import mpmath
 
 
 class PrimeTester:
 
     @staticmethod
-    def perfectPower(n):
-        b = 2
-        while b <= math.log2(n):
-            a = math.pow(n, (1 / b))
-            if a % 1 == 0:
+    def perfectPower(p):
+        # print('Testing the number: ' + str(p))
+        c = 2.0
+        b = mpmath.mpf(2.0)
+        while b <= mpmath.log(p, 2):
+            # print('b is currently ' + str(b))
+            a = mpmath.power(p, (1.0 / b))
+            # print('a is currently ' + str(a))
+            if mpmath.almosteq(a, mpmath.nint(a)):
                 return True
             b += 1
         return False
+
+# n = 2
+# while n < 1000:
+#     if not PrimeTester.perfectPower(math.pow(n, n)):
+#         print("The value " + str(n) + " failed when it shouldn't have.")
+#         break
+#     n += 1
--- UnitTests/UnitTester.py
+++ UnitTests/UnitTester.py
@@ -1,10 +1,30 @@
 import unittest
 from PrimeTester import PrimeTester
+import math
+import mpmath
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_perfectPower_small(self):
         self.assertTrue(PrimeTester.PrimeTester.perfectPower(8))
+        self.assertTrue(PrimeTester.PrimeTester.perfectPower(3125))
+        self.assertFalse(PrimeTester.PrimeTester.perfectPower(7))
+        self.assertTrue(PrimeTester.PrimeTester.perfectPower(343))
+        self.assertTrue(PrimeTester.PrimeTester.perfectPower(823543))
+
+    def test_perfectPower_large(self):
+        self.assertTrue(PrimeTester.PrimeTester.perfectPower(math.pow(25, 25)))
+        self.assertFalse(PrimeTester.PrimeTester.perfectPower(math.pow(25, 25)) - 1)
+        self.assertTrue(PrimeTester.PrimeTester.perfectPower(math.pow(100, 100)))
+        self.assertFalse(PrimeTester.PrimeTester.perfectPower(mpmath.power(1001, 1001))-1)
+
+    def test_perfectPower_loop(self):
+        n = 2
+
+        while n < 10000:
+            print('Testing the number: ' + str(n))
+            self.assertTrue(PrimeTester.PrimeTester.perfectPower(mpmath.power(n, n)), "The value " + str(n) + " failed")
+            n += 1
 
 
 if __name__ == '__main__':
